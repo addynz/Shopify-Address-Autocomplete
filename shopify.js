fb07deb8420303e88ee19c4a39ec5e3aad1cffa4
addynz <jay@addy.co.nz>
2017-09-28 20:23:22 +1300

    Add files via upload

---
+++ shopify.js
@@ -0,0 +1,7 @@
+﻿(function (d, w) {
+    var s = document.createElement("SCRIPT");
+    s.src = 'https://addycdn.azureedge.net/shopify/1.1.0/addycomplete.min.js?key=YOUR-ADDY-KEY';
+    s.type = 'text/javascript';
+    s.async = 1;
+    d.body.appendChild(s);
+})(document, window);
